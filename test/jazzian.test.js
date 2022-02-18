04e278967ac01fafb2015fad05e5ec035ffef4df
tobi-adegbuji-moonpi <tobi.adegbuji@accenture.com>
2022-02-17 23:23:46 -0500

    first commit

---
+++ test/jazzian.test.js
@@ -0,0 +1,13 @@
+const Jazzian = artifacts.require("Jazzian");
+
+/*
+ * uncomment accounts to access the test accounts made available by the
+ * Ethereum client
+ * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
+ */
+contract("Jazzian", function (/* accounts */) {
+  it("should assert true", async function () {
+    await Jazzian.deployed();
+    return assert.isTrue(true);
+  });
+});
